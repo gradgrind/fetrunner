9023636f2388336b7838a5136835456991c590be
gradgrind <mt.bothfeld@gmail.com>
2025-11-02 06:57:35 +0100

    Remove "accidental" code

--- fet/linux.go
+++ fet/linux.go
@@ -3,17 +3,12 @@
 package fet
 
 import (
-	"fmt"
 	"os"
 )
 
 func init() {
-	fmt.Println("??? 1")
 	fsinfo, err := os.Stat("/dev/shm")
 	if err == nil && fsinfo.IsDir() {
-		fmt.Println("??? 2")
 		TEMPORARY_FOLDER = "/dev/shm/fetrunner"
-	} else {
-		fmt.Println("??? ", err)
 	}
 }
