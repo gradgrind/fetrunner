d64fa0a685a4ee103514ecd8549c08c371b2734e
gradgrind <mt.bothfeld@gmail.com>
2025-11-21 12:02:09 +0100

    Start work on libfetrunner

--- libfetrunner/dispatch.go
+++ libfetrunner/dispatch.go
@@ -1,9 +1,38 @@
 package main
 
 import (
+	"fetrunner/base"
 	"os"
 	"strings"
 )
+
+//TODO: The logger collects entries in a buffer. Should Logger rather be an
+// interface?
+
+type ALogger interface {
+	Enter()
+}
+
+type ThisLogger struct {
+	logchan chan []base.LogEntry
+	logbuf  []base.LogEntry
+}
+
+func NewLogger() ThisLogger {
+	return ThisLogger{
+		logchan: make(chan []base.LogEntry),
+	}
+}
+
+// The loggers must be made available somehow. This map makes them accessible
+// via a tag.
+// TODO: Would an integer key be better?
+var loggerMap map[string]ThisLogger
+
+func init() {
+	// default logger
+	loggerMap = map[string]ThisLogger{"": NewLogger()}
+}
 
 func Dispatch(cmd0 string) string {
 	var result string
